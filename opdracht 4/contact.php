9ac4734219a7db53d8425e19ed9dbf5deb2c0f57
mathtijs <matthijsjurriaans333@gmail.com>
2022-06-27 15:50:55 +0200

    opdracht 4 af

--- opdracht 4/contact.php
+++ opdracht 4/contact.php
@@ -8,6 +8,8 @@ $email = '';
 $bericht = '';
 $datum = date('Y-m-d h:i:s');
 
+$errors = [];
+
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
     
@@ -16,17 +18,37 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     $email = $_POST['email'];
     $bericht = $_POST['bericht'];
 
-    $sql = "INSERT INTO `forms` (`voornaam`, `achternaam`, `email`, `bericht`, `datum`)
-            VALUES (:voornaam, :achternaam, :email, :bericht, :datum);";
-    $statement = $connection->prepare($sql);   
-    $params = [
-        'voornaam' => $voornaam,
-        'achternaam' => $achternaam,
-        'email' => $email,
-        'bericht' => $bericht,
-        'datum' => $datum
-    ];
-    $statement->execute($params);
+    if(isEmpty($voornaam)){
+        $errors['voornaam'] = 'vul uw voornaam in a.u.b';
+    }
+    if(isEmpty($achternaam)){
+        $errors['achternaam'] = 'vul uw achternaam in a.u.b';
+    }
+    if(!isValidEmail($email)){
+        $errors['email'] = 'dit is geen geldig email adres!';
+    }
+    if(!hasMinLength($bericht, 5)){
+        $errors['bericht'] = 'vul minimaal 5 tekens in';
+    }
+
+    if(count($errors) == 0){
+
+        $sql = "INSERT INTO `forms` (`voornaam`, `achternaam`, `email`, `bericht`, `datum`)
+                VALUES (:voornaam, :achternaam, :email, :bericht, :datum);";
+        $statement = $connection->prepare($sql);   
+        $params = [
+            'voornaam' => $voornaam,
+            'achternaam' => $achternaam,
+            'email' => $email,
+            'bericht' => $bericht,
+            'datum' => $datum
+        ];
+        $statement->execute($params);
+
+        header('location: bedankt.html');
+        exit;
+}
+
 }   
 
 
@@ -48,22 +70,42 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
             <h2>have a question?</h2>
         </header>
 
-        <form action="contact.php" method="POST">
+        <form action="contact.php" method="POST" novalidate>
             <div class="form__field">
                 <label for="voornaam">voornaam</label>
-                <input type="text" id="voornaam" name="voornaam" placeholder="voornaam" required>
+                <input type="text" value="<?php echo $voornaam;?>" id="voornaam" name="voornaam" placeholder="voornaam" required>
+                
+                <?php if(!empty($errors['voornaam'])):?>
+                    <p class="form__error"><?php echo $errors['voornaam']?></p>
+                <?php endif;?>
+
             </div>
             <div class="form__field">
                 <label for="achternaam">achternaam</label>
-                <input type="text" id="achternaam" name="achternaam" placeholder="achternaam" required>
+                <input type="text" value="<?php echo $achternaam;?>" id="achternaam" name="achternaam" placeholder="achternaam" required>
+            
+                <?php if(!empty($errors['achternaam'])):?>
+                    <p class="form__error"><?php echo $errors['achternaam']?></p>
+                <?php endif;?>
+            
             </div>
             <div class="form__field">
                 <label for="email">email</label>
-                <input type="email" id="email" name="email" placeholder="email" required>
+                <input type="email" value="<?php echo $email;?>" id="email" name="email" placeholder="email" required>
+            
+                <?php if(!empty($errors['email'])):?>
+                    <p class="form__error"><?php echo $errors['email']?></p>
+                <?php endif;?>
+            
             </div>
             <div class="form__field">
                 <label for="bericht">bericht</label>
-                <textarea id="bericht" name="bericht" placeholder="bericht" required></textarea>
+                <textarea id="bericht" name="bericht" placeholder="bericht" required><?php echo $bericht;?></textarea>
+            
+                <?php if(!empty($errors['bericht'])):?>
+                    <p class="form__error"><?php echo $errors['bericht']?></p>
+                <?php endif;?>
+            
             </div>
             <button type="submit" class="form__button">opsturen</button>
         </form>
